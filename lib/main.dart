c9076af2df6003c5ac629d29fba3f629269517cf
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-08-27 23:41:05 +0530

    Add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:practly/app/speak_ease_app.dart';
@@ -7,7 +10,16 @@ Future<void> main() async {
   final binding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: binding);
   await initializeDeps();
+  _setupCrashlytics();
   FlutterNativeSplash.remove();
 
   runApp(const SpeakEaseApp());
+}
+
+void _setupCrashlytics() {
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 }
