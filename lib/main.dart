b6ab4a6f31c1a38c3544210b2a2fa973f39af83c
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-08-27 14:24:27 +0530

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:practly/app/speak_ease_app.dart';
 import 'package:practly/service_locator/service_locator.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
   await initializeDeps();
+  FlutterNativeSplash.remove();
 
   runApp(const SpeakEaseApp());
 }
