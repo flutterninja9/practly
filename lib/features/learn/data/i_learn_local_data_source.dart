a052ef4758c09a6ea4be5fc1a05be648e1a4605a
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-09-03 18:46:50 +0530

    Remove ununsed code

--- lib/features/learn/data/i_learn_local_data_source.dart
+++ lib/features/learn/data/i_learn_local_data_source.dart
@@ -2,8 +2,6 @@ import 'package:practly/core/enums/enums.dart';
 import 'package:practly/core/models/word/word_of_the_day_model.dart';
 
 abstract class ILearnLocalDataSource {
-  ILearnLocalDataSource(Object object, Object object2);
-
   Future<WordOfTheDayModel?> getWord(Complexity complexity);
 
   Future<void> setWord(WordOfTheDayModel model);
