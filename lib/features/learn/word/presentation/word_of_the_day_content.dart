64a5895ec8e310828bcc1b5f1a8aba728a8af282
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-09-03 19:24:53 +0530

    Update ui of word card

--- lib/features/learn/word/presentation/word_of_the_day_content.dart
+++ lib/features/learn/word/presentation/word_of_the_day_content.dart
@@ -35,8 +35,17 @@ class WordOfTheDayContent extends StatelessWidget {
                   sentence: model.word,
                 ),
               ),
-              ShadBadge.outline(
-                child: Text(model.complexity.name.capitalize()),
+              Row(
+                children: [
+                  ShadBadge.outline(
+                    child: Text(model.complexity.name.capitalize()),
+                  ),
+                  const SizedBox(width: 12),
+                  GestureDetector(
+                    onTap: onRefresh,
+                    child: const Icon(LucideIcons.refreshCcw, size: 20),
+                  ),
+                ],
               ),
             ],
           ),
@@ -50,15 +59,6 @@ class WordOfTheDayContent extends StatelessWidget {
           const SizedBox(height: 20),
           HighlightedSection(title: "Usage:", content: model.usage),
           const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShadButton.secondary(
-                onPressed: onRefresh,
-                child: const Text('Next word'),
-              )
-            ],
-          ),
         ],
       ),
     );
