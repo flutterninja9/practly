38be06df3872193eb0bd4ad2129e32857acb3d47
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-09-04 07:35:45 +0530

    Optimize score calculation logic

--- lib/core/services/score_logic.dart
+++ lib/core/services/score_logic.dart
@@ -1,44 +1,47 @@
 import 'dart:math';
-
 import 'package:practly/core/extensions/string_extensions.dart';
 
 class ScoreLogic {
   int calculateScore(String correctSentence, String userSentence) {
-    int distance = _levenshteinDistance(
-      correctSentence.normalized.toLowerCase(),
-      userSentence.toLowerCase(),
-    );
+    String normalizedCorrect = correctSentence.normalized.toLowerCase();
+    String normalizedUser = userSentence.toLowerCase();
 
-    int maxLength = max(correctSentence.length, userSentence.length);
-
-    // Calculate a normalized score (0.0 to 1.0)
-    double normalizedScore = (1.0 - distance / maxLength);
-
-    // Map normalized score to a score between 1 and 10
-    int score = (normalizedScore * 9 + 1).round();
-
-    return score;
-  }
-
-// Function to calculate Levenshtein distance
-  int _levenshteinDistance(String s1, String s2) {
-    List<List<int>> dp =
-        List.generate(s2.length + 1, (_) => List<int>.filled(s1.length + 1, 0));
-
-    for (int i = 0; i <= s2.length; i++) {
-      for (int j = 0; j <= s1.length; j++) {
-        if (i == 0) {
-          dp[i][j] = j;
-        } else if (j == 0) {
-          dp[i][j] = i;
-        } else if (s2[i - 1] == s1[j - 1]) {
-          dp[i][j] = dp[i - 1][j - 1];
-        } else {
-          dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
-        }
-      }
+    if (normalizedCorrect == normalizedUser) {
+      return 10;
     }
 
-    return dp[s2.length][s1.length];
+    int distance = _levenshteinDistance(normalizedCorrect, normalizedUser);
+    int maxLength = max(normalizedCorrect.length, normalizedUser.length);
+
+    double normalizedScore = (1.0 - distance / maxLength);
+
+    return (normalizedScore * 9 + 1).round();
+  }
+
+  int _levenshteinDistance(String s1, String s2) {
+    int len1 = s1.length;
+    int len2 = s2.length;
+
+    if (len1 == 0) return len2;
+    if (len2 == 0) return len1;
+
+    List<int> previousRow = List<int>.generate(len2 + 1, (index) => index);
+    List<int> currentRow = List<int>.filled(len2 + 1, 0);
+
+    for (int i = 1; i <= len1; i++) {
+      currentRow[0] = i;
+      for (int j = 1; j <= len2; j++) {
+        int cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
+        currentRow[j] = min(
+          min(currentRow[j - 1] + 1, previousRow[j] + 1),
+          previousRow[j - 1] + cost,
+        );
+      }
+      List<int> temp = previousRow;
+      previousRow = currentRow;
+      currentRow = temp;
+    }
+
+    return previousRow[len2];
   }
 }
