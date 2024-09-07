65016db8f0a664518014709d68423ad58f1f1b3f
Anirudh Singh <anirudhisgreat1200@gmail.com>
2024-09-07 18:00:21 +0530

    Update deployment script

--- tools/deploy_cloud_function.sh
+++ tools/deploy_cloud_function.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-npm install -g firebase-tools
+# Create a temporary file for the service account credentials
+echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENT" > $CM_BUILD_DIR/firebase-key.json
+export GOOGLE_APPLICATION_CREDENTIALS="$CM_BUILD_DIR/firebase-key.json"
 firebase use "$FIREBASE_PROJECT_ALIAS"
 firebase deploy --only functions
